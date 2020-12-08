da01d3b86afaeb7f1b6f6611c1b08a62f4703e75
Evelyn Szarek <evelynszarek@gmail.com>
2020-12-08 22:04:54 +0000

    artist_repository functions set up

--- console.py
+++ console.py
@@ -5,6 +5,9 @@ from models.artist import Artist
 import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 
+album_repository.delete_all()
+artist_repository.delete_all()
+
 artist_1 = Artist("David Gilmour")
 artist_repository.add_to_library(artist_1)
 
@@ -19,4 +22,4 @@ album_repository.add_to_library(album_2)
 
 artist_repository.select_all()
 
-pdb.set_trace()
+# pdb.set_trace()
--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -3,6 +3,7 @@ from db.run_sql import run_sql
 from models.album import Album
 
 import repositories.album_repository as album_repository
+import repositories.artist_repository as artist_repository
 
 def add_to_library(album):
     sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
@@ -11,3 +12,40 @@ def add_to_library(album):
     id = results[0]['id']
     album.id = id
     return album
+
+def select_all():
+    albums = []
+
+    sql = "`SELECT * FROM albums"
+    results = run_sql(sql)
+
+    for row in results:
+        artist = artist_repository.select(row['artist_id'])
+        album = Album(row['title'], row['genre'], artist, row['id'])
+        albums.append(album)
+    return albums
+
+def select(id):
+    album = None
+    sql = "SELECT * FROM tasks WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        artist = artist_repository.select(result['artist_id'])
+        album = Album(result['title'], result['genre'], artist, result['id'])
+    return album
+
+def delete_all():
+    sql = "DELETE FROM albums"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM albums WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+def update(album):
+    sql = "UPDATE albums SET (title, genre, artist_id) = (%s, %s, %s) WHERE id = %ss"
+    values = [album.title, album.genre, album.artist.id, album.id]
+    run_sql(sql, values) 
